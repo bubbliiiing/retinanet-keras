377be2091340d3a2fb5c1e38c158114a038e3583
JiaQi Xu <47347516+bubbliiiing@users.noreply.github.com>
2020-05-12 11:56:51 +0800

    Update retinanet_training.py

--- nets/retinanet_training.py
+++ nets/retinanet_training.py
@@ -112,7 +112,7 @@ class Generator(object):
         self.image_size = image_size
         self.num_classes = num_classes
         
-    def get_random_data(self, annotation_line, input_shape, random=True, jitter=.1, hue=.1, sat=1.2, val=1.2, proc_img=True):
+    def get_random_data(self, annotation_line, input_shape, jitter=.1, hue=.1, sat=1.2, val=1.2):
         '''r实时数据增强的随机预处理'''
         line = annotation_line.split()
         image = Image.open(line[0])
