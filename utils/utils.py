11ae229c506c6dd2d44fb3d4441a7d56baa8bc6d
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-06-19 21:41:27 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -72,34 +72,6 @@ class BBoxUtility(object):
         self.ignore_threshold = ignore_threshold
         self._nms_thresh = nms_thresh
         self._top_k = top_k
-        self.boxes = tf.placeholder(dtype='float32', shape=(None, 4))
-        self.scores = tf.placeholder(dtype='float32', shape=(None,))
-        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
-                                                self._top_k,
-                                                iou_threshold=self._nms_thresh)
-        self.sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
-
-    @property
-    def nms_thresh(self):
-        return self._nms_thresh
-
-    @nms_thresh.setter
-    def nms_thresh(self, value):
-        self._nms_thresh = value
-        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
-                                                self._top_k,
-                                                iou_threshold=self._nms_thresh)
-
-    @property
-    def top_k(self):
-        return self._top_k
-
-    @top_k.setter
-    def top_k(self, value):
-        self._top_k = value
-        self.nms = tf.image.non_max_suppression(self.boxes, self.scores,
-                                                self._top_k,
-                                                iou_threshold=self._nms_thresh)
 
     def iou(self, box):
         # 计算出每个真实框与所有的先验框的iou
@@ -228,48 +200,77 @@ class BBoxUtility(object):
         decode_bbox = np.minimum(np.maximum(decode_bbox, 0.0), 1.0)
         return decode_bbox
 
-    def detection_out(self, predictions, mbox_priorbox, keep_top_k=200,
+    def detection_out(self, predictions, mbox_priorbox, background_label_id=0, keep_top_k=200,
                         confidence_threshold=0.4):
         
         # 网络预测的结果
         mbox_loc = predictions[0]
-        # 先验框
-        mbox_priorbox = mbox_priorbox
         # 置信度
         mbox_conf = predictions[1]
+        # 先验框
+        mbox_priorbox = mbox_priorbox
+        
         results = []
         # 对每一个图片进行处理
         for i in range(len(mbox_loc)):
-            results.append([])
+            #------------------------------------------------#
+            #   非极大抑制过程与Retinanet视频中不同
+            #   具体过程可参考
+            #   https://www.bilibili.com/video/BV1Lz411B7nQ
+            #------------------------------------------------#
             decode_bbox = self.decode_boxes(mbox_loc[i], mbox_priorbox)
-            for c in range(self.num_classes):
-                c_confs = mbox_conf[i, :, c]
-                c_confs_m = c_confs > confidence_threshold
-                if len(c_confs[c_confs_m]) > 0:
-                    # 取出得分高于confidence_threshold的框
-                    boxes_to_process = decode_bbox[c_confs_m]
-                    confs_to_process = c_confs[c_confs_m]
-                    # 进行iou的非极大抑制
-                    feed_dict = {self.boxes: boxes_to_process,
-                                    self.scores: confs_to_process}
-                    idx = self.sess.run(self.nms, feed_dict=feed_dict)
-                    # 取出在非极大抑制中效果较好的内容
-                    good_boxes = boxes_to_process[idx]
-                    confs = confs_to_process[idx][:, None]
-                    # 将label、置信度、框的位置进行堆叠。
-                    labels = c * np.ones((len(idx), 1))
-                    c_pred = np.concatenate((labels, confs, good_boxes),
-                                            axis=1)
-                    # 添加进result里
-                    results[-1].extend(c_pred)
 
-            if len(results[-1]) > 0:
-                # 按照置信度进行排序
-                results[-1] = np.array(results[-1])
-                argsort = np.argsort(results[-1][:, 1])[::-1]
-                results[-1] = results[-1][argsort]
-                # 选出置信度最大的keep_top_k个
-                results[-1] = results[-1][:keep_top_k]
+            bs_class_conf = mbox_conf[i]
+            
+            class_conf = np.expand_dims(np.max(bs_class_conf, 1),-1)
+            class_pred = np.expand_dims(np.argmax(bs_class_conf, 1),-1)
+
+            conf_mask = (class_conf >= confidence_threshold)[:,0]
+
+            detections = np.concatenate((decode_bbox[conf_mask], class_conf[conf_mask], class_pred[conf_mask]), 1)
+            unique_class = np.unique(detections[:,-1])
+
+            best_box = []
+            if len(unique_class) == 0:
+                results.append(best_box)
+                continue
+            # 4、对种类进行循环，
+            # 非极大抑制的作用是筛选出一定区域内属于同一种类得分最大的框，
+            # 对种类进行循环可以帮助我们对每一个类分别进行非极大抑制。
+            for c in unique_class:
+                cls_mask = detections[:,-1] == c
+
+                detection = detections[cls_mask]
+                scores = detection[:,4]
+                # 5、根据得分对该种类进行从大到小排序。
+                arg_sort = np.argsort(scores)[::-1]
+                detection = detection[arg_sort]
+                while np.shape(detection)[0]>0:
+                    # 6、每次取出得分最大的框，计算其与其它所有预测框的重合程度，重合程度过大的则剔除。
+                    best_box.append(detection[0])
+                    if len(detection) == 1:
+                        break
+                    ious = iou(best_box[-1],detection[1:])
+                    detection = detection[1:][ious<self._nms_thresh]
+            results.append(best_box)
         # 获得，在所有预测结果里面，置信度比较高的框
-        # 还有，利用先验框和Retinanet的预测结果，处理获得了真实框（预测框）的位置
+        # 还有，利用先验框和retinanet的预测结果，处理获得了真实框（预测框）的位置
         return results
+
+def iou(b1,b2):
+    b1_x1, b1_y1, b1_x2, b1_y2 = b1[0], b1[1], b1[2], b1[3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = b2[:, 0], b2[:, 1], b2[:, 2], b2[:, 3]
+
+    inter_rect_x1 = np.maximum(b1_x1, b2_x1)
+    inter_rect_y1 = np.maximum(b1_y1, b2_y1)
+    inter_rect_x2 = np.minimum(b1_x2, b2_x2)
+    inter_rect_y2 = np.minimum(b1_y2, b2_y2)
+    
+    inter_area = np.maximum(inter_rect_x2 - inter_rect_x1, 0) * \
+                 np.maximum(inter_rect_y2 - inter_rect_y1, 0)
+    
+    area_b1 = (b1_x2-b1_x1)*(b1_y2-b1_y1)
+    area_b2 = (b2_x2-b2_x1)*(b2_y2-b2_y1)
+    
+    iou = inter_area/np.maximum((area_b1+area_b2-inter_area),1e-6)
+    return iou
